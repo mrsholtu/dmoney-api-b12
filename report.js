581a1861a6635f1494d2810cec75311f00ace96e
mrsholtu <showkat7oct@gmail.com>
2025-02-05 21:07:02 +0600

    upload first time

---
+++ report.js
@@ -0,0 +1,23 @@
+const newman = require("newman");
+//require("dotenv").config();
+
+console.log(`API Key: ${process.env.secretKey}`);
+
+newman.run(
+  {
+    collection: `https://documenter.getpostman.com/view/40659331/2sAYQghoo6?access_key=${process.env.secretKey}`,
+    reporters: "htmlextra",
+    iterationCount: 1, 
+    reporter: {
+      htmlextra: {
+        export: "./Reports/report.html", // If not specified, the file will be written to newman/ in the current working directory.
+      },
+    },
+  },
+  function (err) {
+    if (err) {
+      throw err;
+    }
+    console.log("collection run complete!");
+  }
+);
